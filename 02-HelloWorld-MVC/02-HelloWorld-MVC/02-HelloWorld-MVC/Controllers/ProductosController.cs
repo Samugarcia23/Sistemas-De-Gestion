7d8593fe765f0303734751de443cff8c27396fc2
Sam <samugarcia23@gmail.com>
2018-10-16 14:13:27 +0200

    Primeros Ejercicios

---
+++ 02-HelloWorld-MVC/02-HelloWorld-MVC/02-HelloWorld-MVC/Controllers/ProductosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02_HelloWorld_MVC.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		/// <summary>
+		/// Accion del controlador que devuelve la vista listado
+		/// </summary>
+		/// <returns></returns>
+
+		public ActionResult listado()
+		{
+			return View();
+		}
+    }
+}
