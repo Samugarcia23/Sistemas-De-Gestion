73a0646b8b11ebef1bc845aa3a352c38aa20fbb6
Sam <samugarcia23@gmail.com>
2018-11-06 22:01:21 +0100

    Ejercicio BD Arreglado y Ejercicio CRUD comenzado

--- 07-ConexionBD-ASP/07-ConexionBD-ASP/Controllers/HomeController.cs
+++ 07-ConexionBD-ASP/07-ConexionBD-ASP/Controllers/HomeController.cs
@@ -36,38 +36,55 @@ namespace _07_ConexionBD_ASP.Controllers
 
 		public ActionResult ListadoPersonas()
 		{
-			//Aqui va todo el ejercicio pero mas adelante conectaremos con otra clase
+			//Creamos una nueva conexion
 			SqlConnection conexion = new SqlConnection();
+			//Creamos un nuevo comando sql (SQL STATEMENTS)
 			SqlCommand sqlCommand = new SqlCommand();
+			//Creamos un nuevo lector
 			SqlDataReader lector;
+
+			//Creamos un nuevo objeto persona y un objeto lista de personas
 			clsPersona oPersona;
 			List<clsPersona> listado = new List<clsPersona>();
+			//Nos conectamos a la BD de Azure
 			conexion.ConnectionString = "server=serverpersonasdamsg.database.windows.net;database=personasDB;uid=Prueba;pwd=123qweasd!;";
 			try
 			{
+				//Abrimos la conexion
 				conexion.Open();
+				//Creamos nuestro comando select
 				sqlCommand.CommandText = "Select * From Personas";
+				//Asignamos al SQLCommand nuestra conexion SQL
 				sqlCommand.Connection = conexion;
+				//Leemos el comando con el SQLDataReader
 				lector = sqlCommand.ExecuteReader();
+				//Si hay filas...
 				if (lector.HasRows)
 				{
+					//Mientras que el lector pueda leer...
 					while (lector.Read())
 					{
+						//Creamos una nueva persona por cada fila, la cual tendra los datos de nuestra tabla Personas de la BD
 						oPersona = new clsPersona();
-						oPersona.idPersona = (int)lector["idPersona"];
-						oPersona.nombre = (string)lector["nombre"];
-						oPersona.apellidos = (string)lector["apellidos"];
-						oPersona.fechNacimiento = (DateTime)lector["fechNacimiento"];
+						oPersona.idPersona = (int)lector["idPersona"]; //Nombre de los campos de la tabla en la BD
+						oPersona.nombre = (string)lector["nombrePersona"];
+						oPersona.apellidos = (string)lector["apellidosPersona"];
+						oPersona.fechNacimiento = (DateTime)lector["fechaNacimiento"];
 						oPersona.direccion = (string)lector["direccion"];
 						oPersona.telefono = (string)lector["telefono"];
 						oPersona.idDepartamento = (int)lector["idDepartamento"];
+						//Añadimos cada persona al listado de personas
 						listado.Add(oPersona);
 					}
 				}
+				//Cerramos el lector cuando no tenga mas filas por leer
 				lector.Close();
+				ViewData["Estado"] = conexion.State;
 			}
 			catch (SqlException) { ViewData["Estado"] = "Error al intentar conectarse con la BD"; }
+			//Cerramos la conexion SQL
 			finally { conexion.Close(); }
+			//Retornamos el listado a la vista ListadoPersonas
 			return View(listado);
 		}
 	}
--- 08-CRUDPersonas/08-CRUDPersonas-DAL/Listado/clsListadoPersonas.cs
+++ 08-CRUDPersonas/08-CRUDPersonas-DAL/Listado/clsListadoPersonas.cs
@@ -1,6 +1,8 @@
-﻿using _08_CRUDPersonas_Entidades;
+﻿using _08_CRUDPersonas_DAL.Conexion;
+using _08_CRUDPersonas_Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +13,37 @@ namespace _08_CRUDPersonas_DAL.Listado
 	{
 		public List<clsPersona> listadoCompletoPersonas()
 		{
+			clsMyConnection miconexion = new clsMyConnection();
+			SqlConnection sqlconnection = miconexion.getConnection();
+			SqlCommand sqlCommand = new SqlCommand();
+			SqlDataReader lector;
+			clsPersona oPersona;
 			List <clsPersona> lista = new List<clsPersona>();
-
-			//TODO Copiar y pegar el ejercicio del otro dia llamando a MyConnection()
+			string error = "";
+			try
+			{
+				sqlCommand.CommandText = "Select * From Personas";
+				sqlCommand.Connection = sqlconnection;
+				lector = sqlCommand.ExecuteReader();
+				if (lector.HasRows)
+				{
+					while (lector.Read())
+					{
+						oPersona = new clsPersona();
+						oPersona.idPersona = (int)lector["idPersona"];
+						oPersona.nombre = (string)lector["nombrePersona"];
+						oPersona.apellidos = (string)lector["apellidosPersona"];
+						oPersona.fechNacimiento = (DateTime)lector["fechaNacimiento"];
+						oPersona.direccion = (string)lector["direccion"];
+						oPersona.telefono = (string)lector["telefono"];
+						oPersona.idDepartamento = (int)lector["idDepartamento"];
+						lista.Add(oPersona);
+					}
+				}
+				lector.Close();
+			}
+			catch (SqlException) { error = "Error al intentar conectarse con la BD"; }
+			finally { miconexion.closeConnection(ref sqlconnection); }
 
 			return lista;
 		}
