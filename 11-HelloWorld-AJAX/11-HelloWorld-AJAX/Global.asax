e51d42f78cad968e2886c4b23c0efe8c6e5fe00e
Sam <samugarcia23@gmail.com>
2018-12-04 14:36:45 +0100

    Ejemplo ajax

---
+++ 11-HelloWorld-AJAX/11-HelloWorld-AJAX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_11_HelloWorld_AJAX.MvcApplication" Language="C#" %>
---
+++ 11-HelloWorld-AJAX/11-HelloWorld-AJAX/Scripts/RealizarLlamada.js
@@ -0,0 +1,29 @@
+﻿window.onload = inicializaEventos;
+
+function inicializaEventos() {
+
+    document.getElementById("btnLlamarApi").addEventListener("click", btnLlamar_OnClick, false);
+
+}
+
+
+function btnLlamar_OnClick() {
+    //alert('Holiii');
+    
+    //Paso 1
+    var llamada = new XMLHttpRequest();
+    //Paso 2
+    llamada.open('GET', "https://10-apirestpersonas-sam.azurewebsites.net/api/personas");
+    //Paso 4
+    llamada.onreadystatechange = function () {
+        alert(llamada.readyState);
+        if (llamada.readyState < 4) {
+            document.getElementById("textoMostrar").innerHTML = "Cargando. . .";
+        } else if (llamada.readyState == 4 && llamada.status == 200) {
+            document.getElementById("textoMostrar").innerHTML = llamada.responseText;
+        }
+    };
+    //Paso 5
+    llamada.send();
+
+}
