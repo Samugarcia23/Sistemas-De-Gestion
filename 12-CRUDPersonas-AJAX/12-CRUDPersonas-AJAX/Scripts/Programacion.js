b79bc3d3f0ba4e7c1f71df487fedfabfb7da45a4
Sam <samugarcia23@gmail.com>
2018-12-12 14:34:41 +0100

    Continuacion CRUD AJAX

---
+++ 12-CRUDPersonas-AJAX/12-CRUDPersonas-AJAX/Scripts/Programacion.js
@@ -0,0 +1,47 @@
+﻿window.onload = inicializaEventos;
+
+function inicializaEventos() {
+
+    cargar();
+
+}
+
+function cargar() {
+
+    var llamada = new XMLHttpRequest();
+    llamada.open('GET', "https://10-apirestpersonas-sam.azurewebsites.net/api/personas");
+    llamada.onreadystatechange = function () {
+        if (llamada.readyState == 4 && llamada.status == 200) {
+            var arrayPersonas = JSON.parse(llamada.responseText);
+            rellenarTabla(arrayPersonas);
+        }
+    };
+    llamada.send();
+}
+
+function rellenarTabla(arrayPersonas) {
+
+    var body = document.getElementsByTagName("body")[0];
+    var tabla = document.createElement("table");
+    tabla.setAttribute("border", "1");
+
+    var tblBody = document.createElement("tbody");
+
+    for (var i = 0; i < arrayPersonas.length; i++) {
+
+        var row = document.createElement("tr");
+
+        for (var prop in arrayPersonas[0]) {
+
+            var column = document.createElement("td");
+            var textoCelda = document.createTextNode(arrayPersonas[i][prop]);
+            column.appendChild(textoCelda);
+            row.appendChild(column);
+        }
+
+        tblBody.appendChild(row);
+    }
+
+    tabla.appendChild(tblBody);
+    body.appendChild(tabla); 
+}
---
+++ 12-CRUDPersonas-AJAX/12-CRUDPersonas-AJAX/Scripts/clsPersona.js
@@ -0,0 +1,10 @@
+﻿class clsPersona {
+    constructor(nombre, apellidos, fechnac, direccion, telefono, iddep) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechNacimiento = fechnac;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.iddepartamento = iddep;
+    }
+}
